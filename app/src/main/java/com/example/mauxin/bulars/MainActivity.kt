8153ccce06ce44538f5e8433bfed2d303dd29c2b
Mauxin <murilo.mdm@hotmail.com>
2019-01-22 14:33:29 -0200

    firebase configurated

--- app/src/main/java/com/example/mauxin/bulars/MainActivity.kt
+++ app/src/main/java/com/example/mauxin/bulars/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.example.mauxin.bulars
 
-import android.content.ContentValues
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var analytics: FirebaseAnalytics? = null
+    private var db: FirebaseFirestore? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        searchCameraButton.setOnClickListener { openCameraManager() }
+        analytics = FirebaseAnalytics.getInstance(this)
+        db = FirebaseFirestore.getInstance()
+        searchCameraButton.setOnClickListener { changeText() }
     }
 
-    private fun openCameraManager() {
-        val values = ContentValues(1)
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
-        val fileUri = contentResolver
-            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                values)
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if(intent.resolveActivity(packageManager) != null) {
-            mCurrentPhotoPath = fileUri.toString()
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
-            intent.addFlags(
-                Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
-        }
+    fun changeText() {
+        logClickEvent()
+
+        db?.collection("medicates")
+            ?.whereEqualTo("name", "benalet")
+            ?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result) {
+                       recognizedTextView.text = document["medicineBottle"].toString()
+                    }
+                } else {
+                    recognizedTextView.text = "Erro"
+                }
+            }
+    }
+
+    fun logClickEvent() {
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "android_button_click")
+        analytics?.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
